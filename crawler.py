101ec8d9e6f076694bb9cdc22e18612008f6f3d6
mgruben <GrubenM@gmail.com>
2016-10-20 22:26:47 -0500

    Revert "Add raw description to the pandas dataframe"
    This reverts commit 02c83ac18971dc917396a9d6ed0db28151063e9c.

--- crawler.py
+++ crawler.py
@@ -149,9 +149,6 @@ def get_attrs(lnk):
     p_list = soup.find_all('p')
     spans = soup.find_all('span')
     
-    # store the extended posting description
-    sect = soup.find_all(id="postingbody")
-    
     # Set the price of the item from the attribute.
     # If price is not listed, set it to NaN.
     try:
@@ -166,7 +163,6 @@ def get_attrs(lnk):
     # Create a dictionary of the attributes of the item in the format {'attribute': 'value', ...}
     d = {}
     d['Price'] = price
-    d['Description'] = sect
     for attr in attr_list:
         try:
             d[attr.contents[0]] = attr.contents[1].contents[0]
